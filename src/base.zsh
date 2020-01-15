ef0524c7a6919c23104e6ee73e68b9b27323b7ea
@slovacus <slovacus@gmail.com>
2020-01-15 18:23:37 -0500

    chore: implement dependence terragrunt (#11)

--- src/base.zsh
+++ src/base.zsh
@@ -1,3 +1,16 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
+function terragrunt::install {
+    if ! type -p brew > /dev/null; then
+        message_warning "${TFENV_MESSAGE_BREW}"
+        return
+    fi
+    message_info "Installing terragrunt"
+    brew install terragrunt
+    message_success "Installed terragrunt"
+}
+
+if ! type -p terragrunt > /dev/null; then
+    terragrunt::install
+fi
