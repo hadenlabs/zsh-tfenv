9dbab699b73156957dd25c8085ee3ca995078436
Luis Mayta <luis@hadenlabs.com>
2020-03-05 18:24:13 -0500

    chore: implement terraform docs (#16)

--- src/base.zsh
+++ src/base.zsh
@@ -11,6 +11,21 @@ function terragrunt::install {
     message_success "Installed terragrunt"
 }
 
+function terraform-docs::install {
+    if ! type -p brew > /dev/null; then
+        message_warning "${TFENV_MESSAGE_BREW}"
+        return
+    fi
+    message_info "Installing terraform docs"
+    brew install terraform-docs
+    message_success "Installed terraform docs"
+}
+
+
 if ! type -p terragrunt > /dev/null; then
     terragrunt::install
+fi
+
+if ! type -p terraform-docs > /dev/null; then
+    terraform-docs::install
 fi
