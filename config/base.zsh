216b0595546920a2a68f57598b8c94fda9b06b6f
@slovacus <slovacus@gmail.com>
2021-06-28 00:26:10 -0500

    chore: implement version 1.0.1 terraform (#41)

--- config/base.zsh
+++ config/base.zsh
@@ -7,10 +7,10 @@ export TFENV_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/
 export TF_PLUGIN_CACHE_DIR="${HOME}"/.terraform.d/plugin-cache
 export TFENV_PACKAGE_NAME=tfenv
 export TFENV_VERSIONS=(
-    0.12.29
     0.13.0
     0.14.10
     0.15.0
     0.15.1
+    1.0.1
 )
-export TFENV_VERSION_GLOBAL=0.15.1
+export TFENV_VERSION_GLOBAL=1.0.1
